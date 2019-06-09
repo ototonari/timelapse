797022674c0a3bd66f682d37d3ed835656fb994e
tsubasa <sinnyo.tsubasa@gmail.com>
2019-06-09 21:02:35 +0900

    updates

---
+++ example/src/lib/Utils.ts
@@ -0,0 +1,20 @@
+import { ImageProps } from "../component/Timelapse";
+
+export const sampleImageCreator = (maxIndex: number) => {
+  const assetsPath = 'assets/sample/'
+  const extension = '.jpg'
+  function zeroPadding(num: number, length: number){
+    return ('0000000000' + num).slice(-length);
+  }
+  const size = new Array(maxIndex).fill(0)
+  const images: ImageProps[] = size.map((n, index) => {
+    const No = zeroPadding(index + 1, 4)
+    const image: ImageProps = {
+      src: assetsPath + 'img' + No + extension,
+      alt: 'sample'
+    }
+    return image
+  })
+  console.log(images)
+  return images
+}
